beb8ebc313f55cbd01bb647f1db4e5aefea8b7d5
syery <9673yuki@gmail.com>
2019-02-23 16:59:59 +0900

    円をついかできた

--- app/src/main/java/com/example/dojo/canvasapp01/MyView.java
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -3,21 +3,34 @@ package com.example.dojo.canvasapp01;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 public class MyView extends View {
+    private Paint paint;
 
 
     //コンストラクタ(初期設定
     public MyView(Context Con) {
         super(Con);
+
+        paint = new Paint();
     }
+
     //絵画の処理
     @Override
     protected void onDraw(Canvas canvas){
 
         //背景
         canvas.drawColor(Color.BLUE);
+
+        //円
+        paint.setColor(Color.MAGENTA);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(25);
+        canvas.drawCircle(300,500,90,paint);
+        paint.setColor(Color.GREEN);
+        canvas.drawCircle(400,500,90,paint);
     }
 
 
