f9cb77ad884332ba9cad4064c08dc82a5f451a80
syery <9673yuki@gmail.com>
2019-02-23 15:54:44 +0900

    キャンバスを使った表示クラスを追加

--- app/src/main/java/com/example/dojo/canvasapp01/CanvasApp01.java
+++ app/src/main/java/com/example/dojo/canvasapp01/CanvasApp01.java
@@ -8,6 +8,7 @@ public class CanvasApp01 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_canvas_app01);
+ //       setContentView(R.layout.activity_canvas_app01);
+
     }
 }
---
+++ app/src/main/java/com/example/dojo/canvasapp01/MyView.java
@@ -0,0 +1,14 @@
+package com.example.dojo.canvasapp01;
+
+import android.content.Context;
+import android.view.View;
+
+public class MyView extends View {
+
+
+    //コンストラクタ(初期設定
+    public MyView(Context Con) {
+        super(Con);
+    }
+
+}
