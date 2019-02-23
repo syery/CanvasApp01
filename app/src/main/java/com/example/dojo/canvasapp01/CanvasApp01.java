96dc21d1cf0011c12ecaaf7c8c5762cfe05a7ee9
syery <9673yuki@gmail.com>
2019-02-23 14:55:43 +0900

    ファーストコミット

---
+++ app/src/main/java/com/example/dojo/canvasapp01/CanvasApp01.java
@@ -0,0 +1,13 @@
+package com.example.dojo.canvasapp01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CanvasApp01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas_app01);
+    }
+}
